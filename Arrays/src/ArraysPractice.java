5394cfb05b94bc7befbd7ed3bcb89934493819d6
Josh Archer <jarcher@greenriver.edu>
2018-02-21 11:53:00 -0800

    Added examples for allocating memory for arrays and assigning, retrieving values in an array. Added an example of graphics objects using arrays.

---
+++ Arrays/src/ArraysPractice.java
@@ -0,0 +1,91 @@
+import acm.util.RandomGenerator;
+
+public class ArraysPractice 
+{
+	public static void main(String[] args)
+	{
+		String[] movies = new String[6];
+		
+		movies[0] = "Back to the Future";
+		movies[1] = "Rocky";
+		movies[2] = "Too Wong Foo";
+		movies[3] = "Jurassic Park";
+		movies[4] = "American History X";
+		movies[5] = "Eternal Sunshine of the Spotless Mind";
+		
+		for (int i = 0; i < movies.length; i++)
+		{
+			System.out.println(movies[i].replace("e", "eee").
+					                     replace("E", "EEE"));
+		}
+		
+		String[] hobbies = {"watching movies", "", "coding", "hiking", "video games"};
+		for (int i = 0; i < hobbies.length; i++)
+		{
+			System.out.println(hobbies[i]);
+		}
+	}
+	
+	public void randomArray()
+	{
+		//create a random array of numbers
+		RandomGenerator random = new RandomGenerator();
+		double[] randomNumbers = new double[1000000];
+		
+		//assign the elements in my array using a loop
+		for (int i = 0; i < randomNumbers.length; i++)
+		{
+			randomNumbers[i] = random.nextDouble(1, 1000000);
+		}
+		
+		//print the values in my array using a loop
+		for (int i = 0; i < randomNumbers.length; i++)
+		{
+			System.out.println(i + ": " + randomNumbers[i]);
+		}
+		
+		//calculate the average of all elements in the randomNumbers array
+		double total = 0;
+
+		for (int i = 0; i < randomNumbers.length; i++)
+		{
+			total += randomNumbers[i];
+		}
+		System.out.println();
+		System.out.println("Average: " + total / randomNumbers.length);
+	}
+	
+	public void creatingArrays()
+	{
+		//step #1 - allocate memory for the array
+		int[] numbers = new int[5];
+		
+		//step #2 - assign spaces in the array
+		numbers[0] = 10;
+		numbers[4] = 20;
+		//numbers[-1] = 30; //this will result in an index-out-of-bounds
+		
+		//step #3 - retrieve elements at an index
+		System.out.println("First element: " + numbers[0]);
+		
+		//search for an element
+		for (int i = 0; i < numbers.length; i++)
+		{
+			int element = numbers[i];
+			
+			if (element == 20)
+			{
+				System.out.println("Found the number 20!");
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ Arrays/src/BouncingBalls.java
@@ -0,0 +1,84 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+public class BouncingBalls extends GraphicsProgram
+{
+	private static final int MOVEMENT = 1;
+	private static final int ANIMATION_PAUSE = 20;
+	private static final int BALL_SIZE = 5;
+	private static final int NUM_BALLS = 1000;
+	private static final int WIN_SIZE = 400;
+
+	public void init()
+	{
+		setSize(WIN_SIZE, WIN_SIZE);
+		setTitle("Bouncing Balls!");
+	}
+	
+	public void run()
+	{
+		RandomGenerator random = new RandomGenerator();
+		
+		//make space for 100 balls
+		GOval[] balls = new GOval[NUM_BALLS];
+		int[] dxs = new int[NUM_BALLS];
+		int[] dys = new int[NUM_BALLS];
+		
+		//create the 100 balls
+		for (int i = 0; i < balls.length; i++)
+		{
+			//create a random position
+			int x = random.nextInt(0, WIN_SIZE - BALL_SIZE);
+			int y = random.nextInt(0, WIN_SIZE - BALL_SIZE);
+			
+			//assign a random dx and dy value for the associated ball at index i
+			while (dxs[i] == 0 && dys[i] == 0)
+			{
+				dxs[i] = random.nextInt(-MOVEMENT, MOVEMENT);
+				dys[i] = random.nextInt(-MOVEMENT, MOVEMENT);
+			}
+			
+			//create the ball
+			GOval ball = new GOval(x, y, BALL_SIZE, BALL_SIZE);
+			ball.setFilled(true);
+			ball.setFillColor(random.nextColor());
+			
+			//save the ball and add it to our array
+			add(ball);
+			balls[i] = ball;
+		}
+		
+		//animate
+		while (true)
+		{
+			//update the position of each ball!
+			for (int i = 0; i < balls.length; i++)
+			{
+				GOval ball = balls[i];
+				int dx = dxs[i];
+				int dy = dys[i];
+				
+				ball.move(dx, dy);
+				
+				//check whether the ball hit the edge of the window
+				if (ball.getX() <= 0 || ball.getX() + ball.getWidth() >= WIN_SIZE)
+				{
+					dxs[i] = -dx;
+				}
+				
+				if (ball.getY() <= 0 || ball.getY() + ball.getHeight() >= WIN_SIZE)
+				{
+					dys[i] = -dy;
+				}
+			}
+			pause(ANIMATION_PAUSE);
+		}
+	}
+}
+
+
+
+
+
+
